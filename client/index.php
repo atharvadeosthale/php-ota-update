4f730949742900e2ee700109035888069367eee2
atharvadeosthale <atharva.deosthale17@gmail.com>
2019-08-18 10:25:34 -0700

    Update index.php

--- client/index.php
+++ client/index.php
@@ -4,18 +4,26 @@ require_once "functions.php";
 
 $url = "http://localhost/newota/server/";
 
+// set POST data
+
 $data = [
 "version" => "1.0"
 ];
 
 $datastring = http_build_query($data);
 
+// open connection
+
 $connection = curl_init();
+
+// set cURL options
 
 curl_setopt($connection, CURLOPT_URL, $url);
 curl_setopt($connection, CURLOPT_POST, true);
 curl_setopt($connection, CURLOPT_POSTFIELDS, $datastring);
 curl_setopt($connection, CURLOPT_RETURNTRANSFER, true);
+
+// establish connection and execute instruction and capture the response
 
 $response = curl_exec($connection);
 
@@ -30,9 +38,11 @@ else
 
 echo "New version found!<br>Updating Product....";
 
+// Download update package
+
 file_put_contents("update.zip", file_get_contents($response["status_message"]));
 
-// Get Project path
+// Get project path
 define('_PATH', dirname(__FILE__));
 
 // Zip file name
