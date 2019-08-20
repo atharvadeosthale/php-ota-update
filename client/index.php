ade5cf15e1b72868bd7c3714cf545df5446aca9d
atharvadeosthale <atharva.deosthale17@gmail.com>
2019-08-20 03:01:12 -0700

    Update index.php

--- client/index.php
+++ client/index.php
@@ -1,45 +1,50 @@
 <?php
 
+// OTA Update Script
+// Made by Atharva Deosthale
+
+// Require functions file
 require_once "functions.php";
 
+// Set cURL URL
 $url = "http://localhost/newota/server/";
 
 // set POST data
-
 $data = [
 "version" => "1.0"
 ];
 
+// Create HTTP POST Data
 $datastring = http_build_query($data);
 
 // open connection
-
 $connection = curl_init();
 
 // set cURL options
-
-curl_setopt($connection, CURLOPT_URL, $url);
-curl_setopt($connection, CURLOPT_POST, true);
-curl_setopt($connection, CURLOPT_POSTFIELDS, $datastring);
-curl_setopt($connection, CURLOPT_RETURNTRANSFER, true);
+curl_setopt($connection, CURLOPT_URL, $url); // url
+curl_setopt($connection, CURLOPT_POST, true); // POST
+curl_setopt($connection, CURLOPT_POSTFIELDS, $datastring); // set data for post
+curl_setopt($connection, CURLOPT_RETURNTRANSFER, true); // return data without echoing
 
 // establish connection and execute instruction and capture the response
-
 $response = curl_exec($connection);
 
+// check response for updates
 if($response["status_code"] == "200")
 {
+	// save link
 	$link = $response["status_message"];
 }
 else
 {
+	// kill app 
 	die("You are already using the latest version of product");
 }
 
+// Start update
 echo "New version found!<br>Updating Product....";
 
 // Download update package
-
 file_put_contents("update.zip", file_get_contents($response["status_message"]));
 
 // Get project path
@@ -63,6 +68,7 @@ if ($res === TRUE) {
  echo 'failed!';
 }
 
+// start update process
 header("Location: update_files/update.php");
 die();
 
