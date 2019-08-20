e1c6d10c611d347d7a17639817f9fb6941a1a559
atharvadeosthale <atharva.deosthale17@gmail.com>
2019-08-20 02:56:58 -0700

    Update functions.php

--- server/functions.php
+++ server/functions.php
@@ -1,10 +1,18 @@
 <?php
 
+// OTA Update Script
+// Made by Atharva Deosthale
+
+// Function to send response to the client in the JSON Format
+
 function send_response($x,$y) {
+	// Create response in form of array
 	$response["status_code"] = $x;
 	$response["status_message"] = $y;
 
+	// Encode the data into JSON Format
 	$json = json_encode($response);
+	// RESPOND
 	echo $json;
 	die();
 }
